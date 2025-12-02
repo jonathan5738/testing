41784e0c9e32b1ec7e31d727eeac9d10515db4fd
jonathan5738 <jnakahonda@gmail.com>
2025-12-02 13:57:30 +0100

    imported postController and userController inside index.js file and added routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 require("dotenv").config()
 const { connectToDatabase } = require("./db");
+const postController = require("./controllers/postController");
+const userController = require("./controllers/userController");
 const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
+
+app.get("/api/posts", postController.getAllPosts);
+app.get("/api/posts/:id", postController.getOnePosts);
+app.post("/api/posts", postController.createPost);
 
 app.listen(PORT, async () => {
     await connectToDatabase();
